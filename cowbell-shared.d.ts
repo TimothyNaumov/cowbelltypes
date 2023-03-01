80948450d2337085b10d64ab6c5c8092936e12c7
Timothy Naumov <timothynaumov@timothyumovsmbp.taile0fcb.ts.net>
2023-03-01 15:54:06 -0600

    new pokemon type

--- cowbell-shared.d.ts
+++ cowbell-shared.d.ts
@@ -1,12 +1,13 @@
 /// <reference types="react" />
 export type Pokemon = {
-    name: string;
-    type: string;
+  name: string;
+  type: string;
+  favoriteColor: string;
 };
 type PokemonListFunction = (nameFilter: string) => Pokemon[];
 export declare function sharedFunction(): string;
 export type PokemonListComponent = React.FC<{
-    list: Pokemon[];
+  list: Pokemon[];
 }>;
 export declare const pokemonTypeFilter: PokemonListFunction;
 export { CowbellThemeProvider } from "./styleguide/cowbell-styles";
